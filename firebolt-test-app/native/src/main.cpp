4354e7039bea1a57c85c6d2bc78775f9b1c1273b
Arun Madhavan <arun_madhavan@comcast.com>
2026-08-21 07:48:13 -0400

    Main app exit refactor

--- firebolt-test-app/native/src/main.cpp
+++ firebolt-test-app/native/src/main.cpp
@@ -730,11 +730,7 @@ int main(int argc, char** argv)
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
-    if (lifecycleSubId != 0) {
-        Firebolt::IFireboltAccessor::Instance().LifecycleInterface().unsubscribe(lifecycleSubId);
-    }
-
-    Firebolt::IFireboltAccessor::Instance().Disconnect();
+    Firebolt::IFireboltAccessor::Instance().LifecycleInterface().close(Firebolt::Lifecycle::CloseType::UNLOAD);
     log_info("Exit complete.");
 
     return 0;
